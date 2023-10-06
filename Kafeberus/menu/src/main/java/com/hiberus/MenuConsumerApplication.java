fa170d0d9f23428e92e1c70f4cefcf113ab3d15d
Lisardo Carretero Colmenar <lcarretero@hiberus.com>
2023-10-06 21:07:44 +0200

    table controler/service/sending topic

--- Kafeberus/menu/src/main/java/com/hiberus/MenuConsumerApplication.java
+++ Kafeberus/menu/src/main/java/com/hiberus/MenuConsumerApplication.java
@@ -3,7 +3,7 @@ package com.hiberus;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class MenuConsumerApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
